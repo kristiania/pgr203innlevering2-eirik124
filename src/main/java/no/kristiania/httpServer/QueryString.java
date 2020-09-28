ab27c930b7c32dfcac28e351226f79b1ff25baf6
eirik124 <eirik.lundanes@gmail.com>
2020-09-28 11:23:47 +0200

    Initial Commit

---
+++ src/main/java/no/kristiania/httpServer/QueryString.java
@@ -0,0 +1,7 @@
+package no.kristiania.httpServer;
+
+public class QueryString {
+    public static void main(String[] args) {
+        System.out.print("Hello");
+    }
+}
---
+++ src/test/java/no/kristiania/httpServer/queryStringTest.java
@@ -0,0 +1,10 @@
+package no.kristiania.httpServer;
+
+import org.junit.jupiter.api.Test;
+
+public class queryStringTest {
+    @Test
+    void test() {
+
+    }
+}
